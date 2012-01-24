cda97169d46d718afb353703ddc442026a229e7c
Claus Polanka <claus.polanka@chello.at>
2012-01-24 21:47:02 +0100

    Modified index HTML and added a age text field.

--- src/webaccessibility/client/Main.java
+++ src/webaccessibility/client/Main.java
@@ -10,5 +10,9 @@ public class Main implements EntryPoint {
         TextBox nameField = new TextBox();
         nameField.setText("Enter Name");
         RootPanel.get("nameFieldContainer").add(nameField);
+
+        TextBox ageField = new TextBox();
+        ageField.setText("Enter Age");
+        RootPanel.get("ageFieldContainer").add(ageField);
     }
 }
